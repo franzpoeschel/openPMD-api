0387809ea281684127bb532636e6c4dc4347bc0d
Franz Pöschel <franz.poeschel@gmail.com>
2019-08-18 15:50:59 +0200

    Add chunk assignment facility
    Without implementation for second pass and egalitarian chunk assignment so far.

--- include/openPMD/Streaming.hpp
+++ include/openPMD/Streaming.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <list>
-#include <vector>
+#include <string>
 #include <utility>
+#include <vector>
 
 #include "openPMD/Dataset.hpp"
+#include <unordered_map>
 
 namespace openPMD
 {
 enum class AdvanceStatus
 {
-    OK, OVER
+    OK,
+    OVER
 };
 
 enum class AdvanceMode
@@ -25,7 +28,68 @@ struct ChunkTable
     using T_chunk = std::pair< Offset, Extent >;
     using T_perRank = std::list< T_chunk >;
     using T_chunkTable = std::map< int, T_perRank >;
-    
+
     T_chunkTable chunkTable;
 };
+
+namespace chunk_assignment
+{
+    using RankMeta = std::vector< std::string >;
+
+    struct FirstPass
+    {
+        struct Result
+        {
+            ChunkTable sinkSide;
+            ChunkTable leftOver;
+        };
+
+        virtual Result
+        firstPass( ChunkTable const &, RankMeta const & ) = 0;
+    };
+
+    struct SecondPass
+    {
+        virtual ChunkTable
+        assignLeftovers( FirstPass::Result, RankMeta const & ) = 0;
+    };
+
+    struct SplitEgalitarian
+    {
+        virtual void
+        splitEgalitarian(
+            ChunkTable const & sourceChunks,
+            std::list< int > destinationRanks,
+            ChunkTable & sinkChunks ) = 0;
+
+        virtual ~SplitEgalitarian() = default;
+    };
+
+
+    ChunkTable
+    assignChunks(
+        ChunkTable,
+        RankMeta const &,
+        FirstPass & firstPass,
+        SecondPass & secondPass );
+
+    struct FirstPassByHostname : FirstPass
+    {
+        FirstPassByHostname(
+            RankMeta hostnames,
+            std::unique_ptr< SplitEgalitarian > );
+
+        Result
+        firstPass( ChunkTable const &, RankMeta const & ) override;
+
+    private:
+        std::unique_ptr< SplitEgalitarian > splitter;
+        RankMeta const hostnames;
+        std::unordered_map< std::string, std::list< int > > ranksPerHost;
+
+        void
+        initRanksPerHost();
+    };
+} // namespace chunk_assignment
+
 } // namespace openPMD
