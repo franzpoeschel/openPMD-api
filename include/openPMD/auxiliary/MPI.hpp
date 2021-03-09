71b914abae09f1a4c2e9739f3cd0f12e17a04e7d
Franz Pöschel <franz.poeschel@gmail.com>
2021-03-09 16:25:03 +0100

    Add new MPI helper: collectStringsTo()

---
+++ include/openPMD/auxiliary/MPI.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "openPMD/config.hpp"
+
+#if openPMD_HAVE_MPI
+
+#    include <mpi.h>
+#    include <string>
+#    include <vector>
+
+namespace openPMD
+{
+namespace auxiliary
+{
+    std::vector< std::string >
+    collectStringsTo(
+        MPI_Comm communicator,
+        int destRank,
+        std::string const & thisRankString );
+
+    std::vector< std::string >
+    distributeStringsToAllRanks(
+        MPI_Comm communicator,
+        std::string const & thisRankString );
+} // namespace auxiliary
+} // namespace openPMD
+#endif
