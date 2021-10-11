466849d834e81aa9031feba10e5bcda242e709b2
Franz Pöschel <franz.poeschel@gmail.com>
2021-10-11 11:14:36 +0200

    Correct precedence in ADIOS2: env var vs. JSON param
    JSON params may be specified in arbitrary capitalization. The backend
    should recognize this and not read from environment variables if the
    parameter has already been set via JSON.

--- src/IO/ADIOS/ADIOS2IOHandler.cpp
+++ src/IO/ADIOS/ADIOS2IOHandler.cpp
@@ -2341,7 +2341,8 @@ namespace detail
                             {"adios2", "engine", "parameters", it.key() },
                             "Must be convertible to string type." );
                     }
-                    alreadyConfigured.emplace( it.key() );
+                    alreadyConfigured.emplace(
+                        auxiliary::lowerCase( std::string( it.key() ) ) );
                 }
             }
             auto _useAdiosSteps =
@@ -2368,8 +2369,9 @@ namespace detail
                       << shadow << std::endl;
         }
         auto notYetConfigured =
-            [&alreadyConfigured]( std::string const & param ) {
-                auto it = alreadyConfigured.find( param );
+            [ &alreadyConfigured ]( std::string const & param ) {
+                auto it = alreadyConfigured.find(
+                    auxiliary::lowerCase( std::string( param ) ) );
                 return it == alreadyConfigured.end();
             };
 
