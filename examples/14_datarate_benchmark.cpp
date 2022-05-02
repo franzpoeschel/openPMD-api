9f18c36019fc799cfa05857084009c8abed96c92
Franz Pöschel <franz.poeschel@gmail.com>
2022-05-02 11:09:18 +0200

    Add time printing

--- examples/14_datarate_benchmark.cpp
+++ examples/14_datarate_benchmark.cpp
@@ -1,12 +1,14 @@
 #include <openPMD/openPMD.hpp>
 
+#include <chrono>
+#include <iostream>
 #include <numeric>
 
 int main()
 {
     using field_dt = uint32_t;
-    openPMD::Offset imageOffset{0,0};
-    openPMD::Extent imageExtent{1024, 1024};
+    openPMD::Offset imageOffset{0, 0};
+    openPMD::Extent imageExtent{10240, 1024};
     openPMD::Extent::value_type flattenedExtent = std::accumulate(
         imageExtent.begin(), imageExtent.end(), 1, [](auto left, auto right) {
             return left * right;
@@ -19,11 +21,21 @@ int main()
 QueueLimit = 2
 )";
 
+    double mbytePerShot =
+        (flattenedExtent * numImagesPerShot * sizeof(field_dt)) /
+        double(1024 * 1024);
+    std::cout << "Writing " << mbytePerShot << "MB per iteration." << std::endl;
+
     std::vector<field_dt> baseData(flattenedExtent);
     std::iota(baseData.begin(), baseData.end(), 0);
 
     openPMD::Series write(filename, openPMD::Access::CREATE, tomlConfig);
     openPMD::Dataset ds(openPMD::determineDatatype<field_dt>(), imageExtent);
+
+    using Clock = std::chrono::system_clock;
+    Clock::time_point previous{};
+
+    std::cout << "Durations between iterations in seconds: \n" << std::endl;
 
     for (unsigned currentIteration = 0; currentIteration < numIterations;
          ++currentIteration)
@@ -38,5 +50,14 @@ QueueLimit = 2
             image.storeChunk(baseData, imageOffset, imageExtent);
         }
         iteration.close();
+        Clock::time_point now = Clock::now();
+        if (currentIteration > 0)
+        {
+            auto diffInNanoseconds = (now - previous).count();
+            auto seconds = diffInNanoseconds / 1000000000;
+            auto remainder = diffInNanoseconds % 1000000000;
+            std::cout << seconds << "." << remainder << "s" << std::endl;
+        }
+        previous = now;
     }
 }
