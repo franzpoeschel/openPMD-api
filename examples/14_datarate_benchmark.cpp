655af61b64987503c3d35ec23a00de0b5fa33918
Franz Pöschel <franz.poeschel@gmail.com>
2022-05-03 11:27:57 +0200

    Some cleanup

--- examples/14_datarate_benchmark.cpp
+++ examples/14_datarate_benchmark.cpp
@@ -6,13 +6,12 @@
 
 int main()
 {
+    /*
+     * Configure this block
+     */
     using field_dt = uint32_t;
     openPMD::Offset imageOffset{0, 0};
     openPMD::Extent imageExtent{10240, 5120};
-    openPMD::Extent::value_type flattenedExtent = std::accumulate(
-        imageExtent.begin(), imageExtent.end(), 1, [](auto left, auto right) {
-            return left * right;
-        });
     unsigned numImagesPerShot = 10;
     unsigned numIterations = 25;
     std::string filename = "./stream.sst";
@@ -22,7 +21,13 @@ usesteps = true
 
 [adios2.engine.parameters]
 QueueLimit = 2
+InitialBufferSize = "2100Mb"
 )";
+
+    openPMD::Extent::value_type flattenedExtent = std::accumulate(
+        imageExtent.begin(), imageExtent.end(), 1, [](auto left, auto right) {
+            return left * right;
+        });
 
     double mbytePerShot =
         (flattenedExtent * numImagesPerShot * sizeof(field_dt)) /
@@ -30,6 +35,7 @@ QueueLimit = 2
     std::cout << "Writing " << mbytePerShot << "MB per iteration." << std::endl;
 
     std::vector<field_dt> baseData(flattenedExtent);
+    // Just fill it with some data
     std::iota(baseData.begin(), baseData.end(), 0);
 
     openPMD::Series write(filename, openPMD::Access::CREATE, tomlConfig);
