d5b88ca35da313730aed079ce174cb4bc57defaf
Franz Pöschel <franz.poeschel@gmail.com>
2023-03-23 13:41:07 +0100

    No virtual methods in Container class
    Either this way, or make all of them virtual

--- include/openPMD/backend/Container.hpp
+++ include/openPMD/backend/Container.hpp
@@ -275,7 +275,7 @@ public:
      * @throws  std::out_of_range error if in READ_ONLY mode and key does not
      * exist, otherwise key will be created
      */
-    virtual mapped_type &operator[](key_type const &key)
+    mapped_type &operator[](key_type const &key)
     {
         auto it = container().find(key);
         if (it != container().end())
@@ -310,7 +310,7 @@ public:
      * @throws  std::out_of_range error if in READ_ONLY mode and key does not
      * exist, otherwise key will be created
      */
-    virtual mapped_type &operator[](key_type &&key)
+    mapped_type &operator[](key_type &&key)
     {
         auto it = container().find(key);
         if (it != container().end())
@@ -374,7 +374,7 @@ public:
      * @param   key Key of the element to remove.
      * @return  Number of elements removed (either 0 or 1).
      */
-    virtual size_type erase(key_type const &key)
+    size_type erase(key_type const &key)
     {
         if (Access::READ_ONLY == IOHandler()->m_frontendAccess)
             throw std::runtime_error(
@@ -392,7 +392,7 @@ public:
     }
 
     //! @todo why does const_iterator not work compile with pybind11?
-    virtual iterator erase(iterator res)
+    iterator erase(iterator res)
     {
         if (Access::READ_ONLY == IOHandler()->m_frontendAccess)
             throw std::runtime_error(
