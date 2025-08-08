0a8b0f55aaa34c58422c6e24246dbf8dac754a40
Franz Pöschel <franz.poeschel@gmail.com>
2025-08-08 11:59:33 +0200

    clang-tidy fixes

--- src/RecordComponent.cpp
+++ src/RecordComponent.cpp
@@ -943,7 +943,7 @@ void RecordComponent::loadChunk_impl(
 
         std::optional<T> val =
             switchNonVectorType<detail::do_convert</* To = */ T>>(
-                /* from = */ getDatatype(), rc.m_constantValue);
+                /* dt = */ getDatatype(), rc.m_constantValue);
 
         if (val.has_value())
         {
