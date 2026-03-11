41dfd58b7ffcaf793024ea211fc753898ef0c1bc
Franz Pöschel <franz.poeschel@gmail.com>
2026-03-11 19:01:54 +0100

    Move flushing from storeChunk to resetDataset

--- src/backend/Attributable.cpp
+++ src/backend/Attributable.cpp
@@ -344,14 +344,15 @@ OpenpmdStandard Attributable::openPMDStandard() const
 }
 
 template <bool flush_entire_series>
-void Attributable::seriesFlush_impl(internal::FlushParams const &flushParams)
+void Attributable::seriesFlush_impl(
+    internal::FlushParams const &flushParams, bool flush_io_handler)
 {
-    writable().seriesFlush<flush_entire_series>(flushParams);
+    writable().seriesFlush<flush_entire_series>(flushParams, flush_io_handler);
 }
-template void
-Attributable::seriesFlush_impl<true>(internal::FlushParams const &flushParams);
-template void
-Attributable::seriesFlush_impl<false>(internal::FlushParams const &flushParams);
+template void Attributable::seriesFlush_impl<true>(
+    internal::FlushParams const &flushParams, bool flush_io_handler);
+template void Attributable::seriesFlush_impl<false>(
+    internal::FlushParams const &flushParams, bool flush_io_handler);
 
 void Attributable::flushAttributes(internal::FlushParams const &flushParams)
 {
