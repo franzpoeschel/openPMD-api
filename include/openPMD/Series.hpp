78e2bc1456cb47201344363ac87a1056284cab8e
Franz Pöschel <franz.poeschel@gmail.com>
2026-03-13 18:34:42 +0100

    Continue fixing and breaking things..

--- include/openPMD/Series.hpp
+++ include/openPMD/Series.hpp
@@ -900,7 +900,7 @@ OPENPMD_private
         iterations_iterator end,
         internal::FlushParams const &flushParams,
         bool flushIOHandler = true);
-    void flushRankTable();
+    void flushRankTable(FlushLevel);
     /* Parameter `read_only_this_single_iteration` used for reopening an
      * Iteration after closing it.
      */
