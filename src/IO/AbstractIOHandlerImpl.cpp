9f0f0212e6c204f554ae833f85c2500aaabed922
Franz Pöschel <franz.poeschel@gmail.com>
2026-05-08 17:57:42 +0200

    Recursive reading

--- src/IO/AbstractIOHandlerImpl.cpp
+++ src/IO/AbstractIOHandlerImpl.cpp
@@ -216,6 +216,7 @@ std::future<void> AbstractIOHandlerImpl::flush(FlushLevel l)
                     ", extent=",
                     [&parameter]() { return vec_as_string(parameter.extent); });
                 createDataset(i.writable, parameter);
+                i.writable->objectType = ObjectType::Dataset;
                 break;
             }
             case O::EXTEND_DATASET: {
@@ -229,6 +230,7 @@ std::future<void> AbstractIOHandlerImpl::flush(FlushLevel l)
                     i.writable,
                     "] EXTEND_DATASET");
                 extendDataset(i.writable, parameter);
+                i.writable->objectType = ObjectType::Dataset;
                 break;
             }
             case O::OPEN_FILE: {
@@ -285,6 +287,7 @@ std::future<void> AbstractIOHandlerImpl::flush(FlushLevel l)
                     "] OPEN_DATASET: ",
                     parameter.name);
                 openDataset(i.writable, parameter);
+                i.writable->objectType = ObjectType::Dataset;
                 break;
             }
             case O::DELETE_FILE: {
@@ -344,6 +347,7 @@ std::future<void> AbstractIOHandlerImpl::flush(FlushLevel l)
                     ", extent=",
                     [&parameter]() { return vec_as_string(parameter.extent); });
                 writeDataset(i.writable, parameter);
+                i.writable->objectType = ObjectType::Dataset;
                 break;
             }
             case O::WRITE_ATT: {
@@ -375,6 +379,7 @@ std::future<void> AbstractIOHandlerImpl::flush(FlushLevel l)
                     ", extent=",
                     [&parameter]() { return vec_as_string(parameter.extent); });
                 readDataset(i.writable, parameter);
+                i.writable->objectType = ObjectType::Dataset;
                 break;
             }
             case O::GET_BUFFER_VIEW: {
