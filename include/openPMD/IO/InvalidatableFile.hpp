ff916100a6668cf9f6bb102b6d0425bc414c7c23
David Grote <grote1@llnl.gov>
2026-09-15 14:05:09 -0700

    Fix `std::set` for `InvalidatableFile` in C++20 (#1941)
    * Add spaceship operator for InvalidatableFile
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * C++17 and Consistent
    
    Codex suggestion
    
    * Cleanup
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    Co-authored-by: Franz Pöschel <franz.poeschel@gmail.com>

--- include/openPMD/IO/InvalidatableFile.hpp
+++ include/openPMD/IO/InvalidatableFile.hpp
@@ -69,6 +69,8 @@ struct InvalidatableFile
     std::string *operator->() const;
 
     explicit operator bool() const;
+
+    bool operator<(InvalidatableFile const &f) const;
 };
 } // namespace openPMD
 
@@ -81,15 +83,5 @@ struct hash<openPMD::InvalidatableFile>
     using result_type = std::size_t;
 
     result_type operator()(argument_type const &s) const noexcept;
-};
-
-template <>
-struct less<openPMD::InvalidatableFile>
-{
-    using first_argument_type = openPMD::InvalidatableFile;
-    using second_argument_type = first_argument_type;
-    using result_type = bool;
-    result_type
-    operator()(first_argument_type const &, second_argument_type const &) const;
 };
 } // namespace std
