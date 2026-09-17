1257a304c5cd42091c3d27bdf3501e24a18e3f8f
Franz Pöschel <franz.poeschel@gmail.com>
2026-09-17 10:55:56 +0200

    nodiscard

--- test/CoreTest.cpp
+++ test/CoreTest.cpp
@@ -1327,7 +1327,8 @@ TEST_CASE("unsafe_no_automatic_flush_immediate_flush_test", "[core]")
         .extent(Extent{1})
         .withSharedPtr(storeData)
         .unsafeNoAutomaticFlush()
-        .store();
+        .store()
+        .get();
     REQUIRE(mrc.get().m_chunks.empty());
     o.flush();
 
@@ -1338,7 +1339,8 @@ TEST_CASE("unsafe_no_automatic_flush_immediate_flush_test", "[core]")
         .extent(Extent{1})
         .withSharedPtr(storeData)
         .unsafeNoAutomaticFlush()
-        .store();
+        .store()
+        .get();
     REQUIRE(mrc.get().m_chunks.size() == 1);
     o.flush();
     REQUIRE(mrc.get().m_chunks.empty());
